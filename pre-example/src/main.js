26c11ba2bce2be59e35a78df758f85d37efde7a4
Ismael <ismaproco@gmail.com>
2016-08-19 23:15:52 +0200

    udpated basic example

--- pre-example/src/main.js
+++ pre-example/src/main.js
@@ -1,3 +1,5 @@
+/* required modules */
+
 import Cycle from '@cycle/xstream-run';
 import xs from 'xstream';
 import debounce from 'xstream/extra/debounce';
@@ -9,32 +11,36 @@ function main(sources) {
   // defining the url observer
   // cycle-dom uses the GET method by default
   const _url = 'http://localhost:3001/api/random-quote'
+
+  //create observable for the request using the static url
   const request$ = xs.of({
     url: _url,
     category: 'random-quote'
   });
 
-  // response event which filter by the category
+  // response event which filter by the category defined in the request
   const response$ = sources.HTTP
     .select('random-quote')
     .flatten();
 
-  //updates the virtual dom with the reponse
+  // updates the virtual dom when the response listener is executed
   const vdom$ = response$
     .map(res => res.text) // this is the response text body
-    .startWith('Loading...')
+    .startWith('Loading...') // default value
     .map(text =>
       div('.container', [
         h1(text)
-      ])
+      ]) // return the virtual dom
     );
 
+  // return the virtual dom and the request to be processed by the Cycle.run
   return {
     DOM: vdom$,
     HTTP: request$
   };
 }
 
+// application runner
 Cycle.run(main, {
   DOM: makeDOMDriver('#main-container'),
   HTTP: makeHTTPDriver()
